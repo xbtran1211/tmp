f6b9960fc0256bfb97f559aec97bef4b89fc87c0
Basch Tran <22027453@student.westernsydney.edu.au>
2024-05-17 18:20:55 +1000

    adding a script

---
+++ new.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/demon/Documents/tmp_git/")
+data=read.csv("epiSEIHCRD_combAge.csv")
+data$t = as.Date(data$t, origin = "2020-03-01")
+data_sub= subset(data,subset= t>='2020-07-01' & t <= '2020-12-31')
+plot(data$H, data$t, type='l', col='red', main='')
